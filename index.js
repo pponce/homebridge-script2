f92ba4827b6f7ee905492ec4fafacc93d2e9cbf7
Oleg Malovichko <olegmalovichko@Olegs-MacBook-Pro.local>
2016-09-21 19:43:11 +0300

    added "file-exists"

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var Characteristic;
 var sys = require('sys');
     exec = require('child_process').exec;
     assign = require('object-assign');
+    fileExists = require('file-exists');
 
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
@@ -23,15 +24,9 @@ function scriptAccessory(log, config) {
   this.onCommand = config['on'];
   this.offCommand = config['off'];
   this.stateCommand = config['state'];
-  this.onValue = config['on_value'] || "playing";
+  this.onValue = config['on_value'];
   this.onValue = this.onValue.trim().toLowerCase();
   this.exactMatch = config['exact_match'] || true;
-  this.script = assign({
-    user: config['user'],
-    host: config['host'],
-    password: config['password'],
-    key: config['key']
-  }, config['script']);
 }
 
 scriptAccessory.prototype.matchesString = function(match) {
@@ -49,37 +44,17 @@ scriptAccessory.prototype.setState = function(powerOn, callback) {
   var prop = state + 'Command';
   var command = accessory[prop];
 
-//  var stream = ssh(command, accessory.script);
-
-//  stream.on('error', function (err) {
-//    accessory.log('Error: ' + err);
-//    callback(err || new Error('Error setting ' + accessory.name + ' to ' + state));
- // });
-//
-//  stream.on('finish', function () {
     exec(command, puts);
-    accessory.log('Set ' + accessory.name + ' to ' + 'state000');
+    accessory.log('Set ' + accessory.name + ' to ' + state);
     callback(null);
-//  });
 }
 
 scriptAccessory.prototype.getState = function(callback) {
   var accessory = this;
   var command = accessory['stateCommand'];
-
- // var stream = script(command, accessory.script);
-
-  //stream.on('error', function (err) {
-//    accessory.log('Error: ' + err);
-//    callback(err || new Error('Error getting state of ' + accessory.name));
- // });
-
-  //stream.on('data', function (data) {
-//    var state = data.toString('utf-8').trim().toLowerCase();
     exec(command, puts);
-    accessory.log('State of ' + accessory.name + ' is: ' + 'state000');
-    callback(null, accessory.matchesString('state000'));
-//  });
+    accessory.log('State of ' + accessory.name + ' is: ' + command);
+    callback(null, true);
 }
 
 scriptAccessory.prototype.getServices = function() {
