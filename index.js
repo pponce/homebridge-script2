c15c943098ffede85c1b4d8cd4f0be821eb1b141
pponce <pponce@users.noreply.github.com>
2020-04-18 12:24:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -113,5 +113,5 @@ script2Accessory.prototype.getServices = function() {
     watcher.on('add', fileCreatedHandler);
     watcher.on('unlink', fileRemovedHandler);
   }
-  return [switchService];
+  return [this.informationService, switchService];
 }
