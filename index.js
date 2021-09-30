1e31747f9387b2f35f4cf0accbd57503b0fa9411
MNikl <michel.nikl@gmail.com>
2021-09-30 08:42:19 +0200

    Test push
    Trying to figure out how to connect to this fork with my homebridge setup.

--- index.js
+++ index.js
@@ -1,117 +1,124 @@
 var Service;
 var Characteristic;
+const ServiceTypes = ['Garage Door Opener'];
 
 var exec = require('child_process').exec;
 var assign = require('object-assign');
 var fileExists = require('file-exists');
 var chokidar = require('chokidar');
 
-module.exports = function(homebridge) {
+module.exports = function (homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory('homebridge-script2', 'Script2', script2Accessory);
+  homebridge.registerAccessory('homebridge-script2-Garage-Fork', 'Script2-Garage-Fork', script2Accessory);
 }
 
 function puts(error, stdout, stderr) {
-   console.log(stdout)
+  console.log(stdout)
 }
 
-function script2Accessory(log, config) {
-  this.log = log;
-  this.service = 'Switch';
+class script2Accessory {
 
-  this.name = config['name'];
-  this.onCommand = config['on'];
-  this.offCommand = config['off'];
-  this.stateCommand = config['state'] || false;
-  this.onValue = config['on_value'] || "true";
-  this.fileState = config['fileState'] || false;
-  if (!this.fileState) {
-    this.onValue = this.onValue.trim().toLowerCase();
-  }
-  this.uniqueSerial = config['unique_serial'] || "script2 Serial Number";
-  //this.exactMatch = config['exact_match'] || true;
-}
+  constructor(log, config) {
+    this.log = log;
+    //Deprecated
+    //this.service = 'Switch';
 
-/* 
-  script2Accessory.prototype.matchesString = function(match) {
-  if(this.exactMatch) {
-    return (match === this.onValue);
+    this.name = config['name'];
+    //Accessory type
+    this.service = config['accessory_type'];
+    this.onCommand = config['on'];
+    this.offCommand = config['off'];
+    this.stateCommand = config['state'] || false;
+    this.onValue = config['on_value'] || "true";
+    this.fileState = config['fileState'] || false;
+    if (!this.fileState) {
+      this.onValue = this.onValue.trim().toLowerCase();
+    }
+    this.uniqueSerial = config['unique_serial'] || "script2 Serial Number";
   }
-  else {
-    return (match.indexOf(this.onValue) > -1);
-  }
-}
-*/
 
-script2Accessory.prototype.setState = function(powerOn, callback) {
-  var accessory = this;
-  var state = powerOn ? 'on' : 'off';
-  var prop = state + 'Command';
-  var command = accessory[prop];
+
+
+  setState(powerOn, callback) {
+    var accessory = this;
+    var state = powerOn ? 'on' : 'off'; //This is the old one.
+    var prop = state + 'Command';
+    var command = accessory[prop];
 
     exec(command, puts);
     accessory.log('Set ' + accessory.name + ' to ' + state);
     accessory.currentState = powerOn;
     callback(null);
-}
+  }
 
-script2Accessory.prototype.getState = function(callback) {
-  var accessory = this;
-  
-  if (this.fileState) {
-    var flagFile = fileExists.sync(this.fileState);
-    accessory.log('State of ' + accessory.name + ' is: ' + flagFile);
-    callback(null, flagFile);
-  }
-  else if (this.stateCommand) {
-    exec(this.stateCommand, function (error, stdout, stderr) {
-      if (stderr) { return; }
-      var cleanOut=stdout.trim().toLowerCase();
-      accessory.log('State of ' + accessory.name + ' is: ' + cleanOut);
-      callback(null, cleanOut == accessory.onValue);
-    });
-  }
-  else {
+
+  getState(callback) {
+    var accessory = this;
+
+    if (this.fileState) {
+      var flagFile = fileExists.sync(this.fileState);
+      accessory.log('State of ' + accessory.name + ' is: ' + flagFile);
+      callback(null, flagFile);
+    }
+    else if (this.stateCommand) {
+      exec(this.stateCommand, function (error, stdout, stderr) {
+        if (stderr) { return; }
+        var cleanOut = stdout.trim().toLowerCase();
+        accessory.log('State of ' + accessory.name + ' is: ' + cleanOut);
+        callback(null, cleanOut == accessory.onValue);
+      });
+    }
+    else {
       accessory.log('Must set config value for fileState or state.');
+    }
   }
-}
 
-script2Accessory.prototype.getServices = function() {
-  var informationService = new Service.AccessoryInformation();
-  var switchService = new Service.Switch(this.name);
-  var theSerial = this.uniqueSerial.toString();
 
-  informationService
-  .setCharacteristic(Characteristic.Manufacturer, 'script2 Manufacturer')
-  .setCharacteristic(Characteristic.Model, 'script2 Model')
-  .setCharacteristic(Characteristic.SerialNumber, theSerial);
+  getServices() {
 
-  var characteristic = switchService.getCharacteristic(Characteristic.On)
-  .on('set', this.setState.bind(this));
+    var serviceType = this.service;
 
-  if (this.stateCommand || this.fileState) {
-    characteristic.on('get', this.getState.bind(this))
-  };
-  
-  if (this.fileState) {
-    var fileCreatedHandler = function(path, stats){
-      if (!this.currentState) {
+    if (ServiceTypes.indexOf(serviceType)>=0){
+      this.log('Error! Unable to find accessory type ' + serviceType + ' on the list of supported accessories. Check spelling or if it is supported.');
+      return 0;
+    }
+
+    var informationService = new Service.AccessoryInformation();
+    var switchService = new Service[serviceType](this.name);
+    var theSerial = this.uniqueSerial.toString();
+
+    informationService
+      .setCharacteristic(Characteristic.Manufacturer, 'Script2-Garage-Fork Manufacturer')
+      .setCharacteristic(Characteristic.Model, 'Script2-Garage-Fork Model')
+      .setCharacteristic(Characteristic.SerialNumber, theSerial);
+
+    var characteristic = switchService.getCharacteristic(Characteristic.CurrentDoorState)
+      .on('set', this.setState.bind(this));
+
+    if (this.stateCommand || this.fileState) {
+      characteristic.on('get', this.getState.bind(this))
+    };
+
+    if (this.fileState) {
+      var fileCreatedHandler = function (path, stats) {
+        if (!this.currentState) {
           this.log('File ' + path + ' was created');
-	      switchService.setCharacteristic(Characteristic.On, true);
-      }
-    }.bind(this);
-  
-    var fileRemovedHandler = function(path, stats){
-      if (this.currentState) {
+          switchService.setCharacteristic(Characteristic.On, true);
+        }
+      }.bind(this);
+
+      var fileRemovedHandler = function (path, stats) {
+        if (this.currentState) {
           this.log('File ' + path + ' was deleted');
-	      switchService.setCharacteristic(Characteristic.On, false);
-	  }
-    }.bind(this);
-  
-    var watcher = chokidar.watch(this.fileState, {alwaysStat: true});
-    watcher.on('add', fileCreatedHandler);
-    watcher.on('unlink', fileRemovedHandler);
+          switchService.setCharacteristic(Characteristic.On, false);
+        }
+      }.bind(this);
+
+      var watcher = chokidar.watch(this.fileState, { alwaysStat: true });
+      watcher.on('add', fileCreatedHandler);
+      watcher.on('unlink', fileRemovedHandler);
+    }
+    return [informationService, switchService];
   }
-  return [informationService, switchService];
 }
